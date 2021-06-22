6fe28ae2397faf34bf7b92b201becc8faab62ce6
Cj-bc <cj.bc-sd@outlook.jp>
2021-06-22 23:12:01 +0900

    Add: emacs/lib/org-ghq> making external link for ghq projects [WIP]

---
+++ dotfiles/emacs/emacs.d/lib/org-ghq.el
@@ -0,0 +1,32 @@
+(require 'ol)
+
+(org-link-set-parameters "ghq"
+			 :follow #'org-ghq-open
+			 :export #'org-ghq-export
+			 )
+
+; WIP
+(defun org-ghq--run-shell-command (cmd &rest args)
+  "Run shell command and return its stdout"
+  (setq bn (get-buffer-create "--run-shell-command"))
+  (eval (append '(call-process) (list cmd) '(nil bn nil) args))
+  (setq str (with-current-buffer bn (buffer-string)))
+  (kill-buffer bn)
+  str
+  )
+
+; WIP
+(defun org-ghq-open (name _)
+  "Visit the ghq directory"
+  (setq ghq-project-path (org-ghq--run-shell-command "bash" "-c" (concat "ghq list | grep " name "| tr -d '\n'")))
+  (unless (eq "" ghq-project-path)
+    (let* ((ghq-root-path (org-ghq--run-shell-command "ghq" "root"))
+	  (ghq-full-path (concat ghq-root-path ghq-project-path)))
+      (dired (expand-file-name ghq-full-path))))
+      )
+  
+
+; WIP
+(defun org-ghq-export (link description format _)
+  "Export a ghq project path from org files.
+   It will link to the 'remote' url if available"
