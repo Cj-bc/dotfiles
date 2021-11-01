6b2a0fb63bdb229a21070328d927a46fb8f855bc
Cj-bc <cj.bc-sd@outlook.jp>
2021-11-01 11:37:17 +0900

    Change: emacs/lib/ol-notes> Change license to GPL3; Support title completion
    As I used GPL3-licensed code in this modification, I had to
    make it under GPL3

--- dotfiles/emacs/emacs.d/lib/ol-notes.el
+++ dotfiles/emacs/emacs.d/lib/ol-notes.el
@@ -1,4 +1,28 @@
+;;; ol-notes.el --- External link to manage note files -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2021 Cj.bc-sd a.k.a Cj-bc
+
+;; Author: Cj.bc-sd a.k.a Cj-bc <cj.bc-sd@outlook.jp>
+;; Created: 1 Nov 2021
+;; URL: https://github.com/Cj-bc/dotfiles.git
+
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ; notes:your-org/class-name:2021#title
 
 (require 'ol)
@@ -69,8 +93,20 @@ Accept link without `notes:'
 
 	 (completion-answer (completing-read "<org>/<class>:<year>: "
 					     (seq-map (lambda (p) (ol-notes-path-to-link p)) target-files)))
+	 ;; those codes below is copied from help:pcomplete/org-mode/searchhead
+	 (titles (with-temp-buffer (find-file (ol-notes-link-to-path completion-answer))
+				   (save-excursion
+				     (goto-char (point-min))
+				     (let (tbl)
+				       (while (re-search-forward org-outline-regexp nil t)
+					 ;; Remove the leading asterisk from
+					 ;; `org-link-heading-search-string' result.
+					 (push (substring-no-properties (org-link-heading-search-string) 1) tbl))
+				       (pcomplete-uniquify-list tbl)))))
+	 (title (completing-read "Title(optional): " titles))
 	 )
-    (concat (format "notes:%s" completion-answer)
+    (eval `(concat (format "notes:%s" completion-answer)
+		   ,@(when title '("#" title)))
 	     )))
 
 (provide 'ol-notes)
