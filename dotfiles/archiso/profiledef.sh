74957ad08d985b3250bb10b57cdaa39ba7c6e1cd
Cj-bc <cj.bc-sd@outlook.jp>
2022-04-01 10:03:01 +0900

    Change: archiso/profiledef.sh> Modify metadata

--- dotfiles/archiso/profiledef.sh
+++ dotfiles/archiso/profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="archlinux-x"
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
+iso_publisher="Cj.bc-sd <cj.bc-sd@outlook.jp>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
